0e503231c556fe51c4cda44f8d3fd9f3c809d6f4
Prasantipooja <prasantipooja123@gmail.com>
2025-03-17 22:38:07 +0530

    Add files via upload

---
+++ Temperature_humidity_1_2_sensors_/Temperature_humidity_1_2_sensors_.ino
@@ -0,0 +1,44 @@
+#include <DHT.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);  //0x27 is the i2c address, while 16 = columns, and 2 = rows. 
+
+#define DHTPIN 8 // digital pin connected to sensor
+#define DHTTYPE    DHT11     // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE); 
+
+void setup(){
+  Serial.begin(9600);
+  dht.begin();        //Initialize the DHT11 sensor
+  lcd.begin();        //Initialize the LCD
+  lcd.backlight();    //Activate backlight 
+}
+
+void loop()
+{
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } 
+  
+  lcd.setCursor(0,0); 
+  lcd.print("Temp: ");
+  lcd.print(t);
+  lcd.print((char)223);
+  lcd.print("C");
+  lcd.setCursor(0,1);
+  lcd.print("Humidity: ");
+  lcd.print(h);
+  lcd.print("%");
+  delay(1000); // Delay 1 second, as the DHT11 sampling rate is 1Hz
+}void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
